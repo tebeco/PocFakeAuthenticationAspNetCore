5258c86f518b093d0f96b6747b84278850e007e8
TeBeCo <tebeco@gmail.com>
2018-08-26 15:33:09 +0200

    Login page allow you to login as any user

--- samples/WebApiMultiIndexEls/Pages/Login.cshtml.cs
+++ samples/WebApiMultiIndexEls/Pages/Login.cshtml.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApiMultiIndexEls.Services;
 
 namespace WebApiMultiIndexEls.Pages
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly UserService _userService;
 
+        public LoginModel(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [BindProperty]
+        public string UserName { get; set; }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _userService.LoginAsync(HttpContext, UserName);
+            return RedirectToPage("User");
         }
     }
 }
