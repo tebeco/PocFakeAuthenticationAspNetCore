335c56b15fb38989fdf2c48cc8d425cbbfef079d
TeBeCo <tebeco@gmail.com>
2018-08-26 18:29:58 +0200

    adding a fake user to be always rejected on login attempt (for logs)

--- samples/WebApiMultiIndexEls/Services/UserService.cs
+++ samples/WebApiMultiIndexEls/Services/UserService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -8,12 +9,25 @@ namespace WebApiMultiIndexEls.Services
 {
     public class UserService
     {
-        public async Task LoginAsync(HttpContext httpContext, string userName)
+        public UserService(ILoggerFactory loggerFactory)
         {
+            LoggerFactory = loggerFactory;
+        }
+
+        public ILoggerFactory LoggerFactory { get; }
+
+        public async Task<bool> LoginAsync(HttpContext httpContext, string userName)
+        {
+            if(userName == "deny")
+            {
+                return false;
+            }
+
             var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
             identity.AddClaim(new Claim(ClaimTypes.Name, userName));
 
             await httpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+            return true;
         }
 
         public async Task LogoutAsync(HttpContext httpContext)
